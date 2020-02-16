f68de2d84ad24a049f1c93c8d41161a79d521186
Roger Wang <rogerwangcs@gmail.com>
2020-02-16 07:39:13 -0500

    add server index

--- index.js
+++ index.js
@@ -18,6 +18,17 @@ let gameServer = {
   players: {}
 };
 
+let numClients = 0;
+
+// MASTER GAME LOOP BELOW
+setInterval(() => {
+  if (numClients > 0) {
+    movePlayers();
+    io.emit("updateState", gameServer);
+  }
+}, 50);
+// MASTER GAME LOOP ABOVE
+
 class Player {
   constructor(id, name, size, x, y, xVel, yVel) {
     this.id = id;
@@ -36,13 +47,13 @@ const logServerState = message => {
     console.log(message);
   }
   console.log("\n=====Server State=====");
-  console.log(gameServer.players);
+  console.log(numClients + " clients connected");
   console.log("======================");
 };
 
 const addPlayer = (socketId, playerName) => {
-  let xPos = Math.floor(Math.random() * 500);
-  let yPos = Math.floor(Math.random() * 500);
+  let xPos = Math.floor(Math.random() * (370 - 50) + 50);
+  let yPos = Math.floor(Math.random() * (635 - 50) + 50);
   gameServer.players[socketId] = new Player(
     socketId,
     playerName,
@@ -52,12 +63,36 @@ const addPlayer = (socketId, playerName) => {
     0,
     0
   );
+  numClients++;
 };
 
 const playerQuit = socketId => {
   delete gameServer.players[socketId];
+  numClients--;
   logServerState("Client " + socketId.substring(0, 10) + " left.");
   io.emit("updateState", gameServer);
+};
+
+const movePlayers = () => {
+  Object.keys(gameServer.players).map(playerId => {
+    const player = gameServer.players[playerId];
+    player.x += player.xVel;
+    player.y -= player.yVel;
+
+    // // slow down
+    // if (player.xVel > 0.1) player.xVel - 0.1;
+    // else if (player.xVel < -0.1) player.xVel + 0.1;
+    // if (player.yVel > 0.1) player.yVel - 0.1;
+    // else if (player.yVel < -0.1) player.yVel + 0.1;
+  });
+};
+
+const playerJoystick = (playerId, xJoy, yJoy) => {
+  const player = gameServer.players[playerId];
+  if (player != null) {
+    player.xVel = xJoy / 26;
+    player.yVel = yJoy / 26;
+  }
 };
 
 // Start socket listening
@@ -67,11 +102,23 @@ io.on("connection", socket => {
   logServerState("Client " + socket.id.substring(0, 10) + " joined.");
 
   socket.on("clientGameState", () => {
-    io.emit("updateState", gameServer);
+    io.emit("serverGameState", gameServer);
   });
 
   socket.on("updateState", () => {
     io.emit("updateState", gameServer);
+  });
+
+  socket.on("clientStopPlayer", playerId => {
+    const player = gameServer.players[playerId];
+    if (player != null) {
+      player.xVel = 0;
+      player.yVel = 0;
+    }
+  });
+
+  socket.on("clientJoy", (xJoy, yJoy) => {
+    playerJoystick(socket.id, xJoy, yJoy);
   });
 
   socket.on("disconnect", () => {
